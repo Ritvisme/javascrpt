32b68d0e1ff5b6bfa66d2000dd3eb45d99d9d6e0
Ritvik Bhatnagar <1si23ec092@sit.ac.in>
2024-08-08 00:05:47 +0530

    maps,loops

---
+++ variable/loops/loops.js
@@ -0,0 +1,14 @@
+// for of loop
+const arr = [1, 2, 3, 4, 5];
+for(const sum of arr){
+    console.log(sum);
+
+}//no need to specify the length of the array or the increment value 
+
+//maps are basically key value pairs just like python had dictionaries it also had key value pairs
+
+const map=new Map();
+map.set('name','john');
+map.set('age',30);
+map.set('location','INDIA');
+console.log(map);
