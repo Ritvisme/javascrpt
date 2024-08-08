cb2fd7f095c4e7ef4a3780126f4374798b01becc
Ritvik Bhatnagar <1si23ec092@sit.ac.in>
2024-08-08 23:32:58 +0530

    immidiate functions and their uses

--- variable/IIFE/immediatefunc.js
+++ variable/IIFE/immediatefunc.js
@@ -1,4 +1,6 @@
 //IIFE ARE IMMEDIATELY INVOKED FUNCTION EXPRESSIONS THAT ARE USED , AS SOMETIMES THERE CAN BE POLLUTION FROM THE GLOBAL SCOPE HENCE TO REMOVE THAT POLLUTION OF THE VARIABLES FROM GLOBAL SCOPE WE USE IFFE
+//allows you to create closures, which help maintain data privacy. 
+//. You can hide variables and functions inside an IIFE, exposing only what's necessary. This concept is fundamental to the Module Pattern, a widely-used design pattern in JavaScript.
 
 (function hello(){
     console.log("hello");
