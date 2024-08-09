d57f649985e524bae2d76a41304b9924fb6296eb
Ritvik Bhatnagar <1si23ec092@sit.ac.in>
2024-08-10 00:10:23 +0530

    learning DOMS

---
+++ variable/dom/dom.js
@@ -0,0 +1,8 @@
+//DOM=DOCUMENT OBJECT MODEL When a web page is loaded, the browser creates a Document Object Model of the page.
+
+
+//DOM is a programming interface for HTML and XML documents. It represents the structure of the document as a tree of nodes, where each node represents a part of the document (e.g. an element, text string, or comment).
+//HENCE NODE IS BASICALLY EACH ELEMENT IN THE HTML DOCUMENT
+//DOM is used to manipulate the structure, style, and content of a document. It provides methods and properties for interacting with the document, such as creating, removing, and modifying elements, changing styles, and handling events.
+
+//HTML COLLECTION An HTMLCollection is a collection of document elements. A NodeList is a collection of document nodes (element nodes, attribute nodes, and text nodes). HTMLCollection items can be accessed by their name, id, or index number. NodeList items can only be accessed by their index number.
