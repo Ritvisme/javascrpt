5172eb1d6721dd4d4fbe8e570faa2d3bbdaf72a2
Ritvik Bhatnagar <1si23ec092@sit.ac.in>
2024-08-10 23:26:28 +0530

    dom functions

--- variable/dom/dom.js
+++ variable/dom/dom.js
@@ -1,8 +1,12 @@
-//DOM=DOCUMENT OBJECT MODEL When a web page is loaded, the browser creates a Document Object Model of the page.
+//DOM=DOCUMENT OBJECT MODEL When a web page is loaded, the browser creates a Document Object Model of the page.he Document Object Model (DOM) aims to make it easier for Javascript to access HTML and XML elements
 
 
-//DOM is a programming interface for HTML and XML documents. It represents the structure of the document as a tree of nodes, where each node represents a part of the document (e.g. an element, text string, or comment).
+
 //HENCE NODE IS BASICALLY EACH ELEMENT IN THE HTML DOCUMENT
 //DOM is used to manipulate the structure, style, and content of a document. It provides methods and properties for interacting with the document, such as creating, removing, and modifying elements, changing styles, and handling events.
 
 //HTML COLLECTION An HTMLCollection is a collection of document elements. A NodeList is a collection of document nodes (element nodes, attribute nodes, and text nodes). HTMLCollection items can be accessed by their name, id, or index number. NodeList items can only be accessed by their index number.
+
+//The querySelector() method returns the first child element that matches a specified CSS selector(s) of an element.
+// Note: The querySelector() method only returns the first element that matches the specified selectors. To return all the matches, use the querySelectorAll() method instead.
+document.getElementById('title')
