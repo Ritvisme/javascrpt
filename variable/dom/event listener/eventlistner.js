8a2277af2149662a0ce3cf89821f49baafc28c81
Ritvik Bhatnagar <1si23ec092@sit.ac.in>
2024-08-11 23:59:07 +0530

    event listener

--- variable/dom/event listener/eventlistner.js
+++ variable/dom/event listener/eventlistner.js
@@ -2,3 +2,4 @@
 //The addEventListener() method attaches an event handler to the specified element.
 
 //element.addEventListener(event, function, useCapture);
+//the demo of the event listener is done in the html file 
