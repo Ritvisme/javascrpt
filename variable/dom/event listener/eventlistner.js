56289cf63a730ea0104f6f7af273c6660c6bb019
Ritvik Bhatnagar <1si23ec092@sit.ac.in>
2024-08-11 23:50:20 +0530

    eventlistener

---
+++ variable/dom/event listener/eventlistner.js
@@ -0,0 +1,4 @@
+//event listener in js is a method which is most ed in js to listen to the events like click, mouseover, mouseout, keydown, keyup, keypress, etc.
+//The addEventListener() method attaches an event handler to the specified element.
+
+//element.addEventListener(event, function, useCapture);
