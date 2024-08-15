9c310b64f56d7243f91d308ca27506cf7795811c
Ritvik Bhatnagar <1si23ec092@sit.ac.in>
2024-08-15 23:40:50 +0530

    promises

---
+++ variable/cohort3.0/promises.js
@@ -0,0 +1,11 @@
+// •	Handling Asynchronous Tasks: Callback functions are often used in scenarios where you need to wait for an operation to complete, like reading data from a server or performing an animation.
+// •	Example: Suppose you want to load data from an API and then display it on the webpage. You might use a callback function that gets called after the data is successfully retrieved.
+
+
+// A promise is an object that represents the eventual completion (or failure) of an asynchronous operation and its resulting value. Promises provide a more powerful and readable way to handle asynchronous code compared to callbacks.
+
+
+function callback(){
+    console.log("5 seconds have passed");
+}
+setTimeoutPromisified(5000).then(callback);
---
+++ variable/cohort3.0/settimeout.js
@@ -0,0 +1,4 @@
+function time(){
+    console.log("Hello World");
+}
+setTimeout(time,3000);//it will show hello world after 3 seconds
